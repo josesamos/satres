fc8c5f7cde5c2a3bd41d3d25602d21f546f4674b
josesamos <jsamos@ugr.es>
2023-10-03 21:32:41 +0200

    clip_bands function

--- R/satres.R
+++ R/satres.R
@@ -224,6 +224,52 @@ as_SpatRaster.satres <- function(sr, res = NULL) {
 }
 
 
+#' Clip all the bands based on a polygon
+#'
+#' Clips all bands of each spatial resolution according to the given polygon.
+#'
+#' It performs the operation independently of the CRS of the polygon and preserves
+#' the CRS of the bands.
+#'
+#' @param sr A `satres` object.
+#' @param polygon A `sf` polygon layer.
+#'
+#' @return A `satres` object.
+#'
+#' @family object transformation
+#' @seealso \code{\link{satres}}
+#'
+#' @examples
+#'
+#' file <- system.file("extdata", "lanjaron.gpkg", package = "satres")
+#' lanjaron <- sf::st_read(file, layer = "lanjaron_bbox")
+#'
+#' esa <- system.file("extdata", "esa", package = "satres")
+#' sr <- satres(dir = esa) |>
+#'      clip_bands(polygon = lanjaron)
+#'
+#' @export
+clip_bands <- function(sr, polygon)
+  UseMethod("clip_bands")
+
+#' @rdname clip_bands
+#' @export
+clip_bands.satres <- function(sr, polygon) {
+  first <- TRUE
+  for (b in names(sr$bands)) {
+    if (first) {
+      s <- sf::st_transform(polygon, terra::crs(sr$bands[[b]]))
+      first <- FALSE
+    }
+    r <- terra::crop(sr$bands[[b]], s)
+    sr$bands[[b]] <- terra::mask(r, s)
+  }
+  sr$out_dir <- NULL
+  sr$virtual_files <- NULL
+  sr
+}
+
+
 #' Transforms a list of raster bands in a raster multi-band
 #'
 #' To generate a multi-band raster, the definition of the vector must be
